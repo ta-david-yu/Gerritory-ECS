6346429f82d8180eac1bc5ac9cdfd4c0a07f47a9
Ta David Yu <ta.yu.gamedev@gmail.com>
2022-09-02 23:19:47 +0800

    Renamed CollapseTileOnMoveBeing system file name

--- GerritoryECS/Assets/Scripts/GameFlow/GameConstants.cs
+++ GerritoryECS/Assets/Scripts/GameFlow/GameConstants.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public static class GameConstants
 {
 	public static readonly float TileOffset = 1;
+	public static readonly Quaternion InGameCameraOrientation = new Quaternion(-0.287274003f, -0.227902189f, 0.0705751702f, -0.927660227f);
 
 	public static Vector3 TilePositionToWorldPosition(Vector2Int tilePosition)
 	{
