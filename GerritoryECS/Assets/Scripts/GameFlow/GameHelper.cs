0ed2e29581308b470f36d59a9169a15f488f3581
Ta David Yu <ta.yu.gamedev@gmail.com>
2022-09-11 22:59:03 +0800

    Moved State removal logic to a separate function

--- GerritoryECS/Assets/Scripts/GameFlow/GameHelper.cs
+++ GerritoryECS/Assets/Scripts/GameFlow/GameHelper.cs
@@ -90,4 +90,27 @@ public static class GameHelper
 
 		return TryGetValidRespawnPositionFor(contexts, entity);
 	}
+
+
+	private static readonly PlayerStateEntity[] s_PreallocatedPlayerStateEntitiesToBeDestroyed = new PlayerStateEntity[4];
+	public static void RemovePlayerStateFor(this PlayerStateContext context, int stateHolderId)
+	{
+		var playerStateEntitiesSet = context.GetEntitiesWithState(stateHolderId);
+		if (playerStateEntitiesSet.Count > 1)
+		{
+			Debug.LogWarning($"There should only be at most 1 state targetting a state holder at the same time, but there are {playerStateEntitiesSet.Count}." +
+				$"Something could be wrong!");
+		}
+
+		// We need to do copy the list because playerStateEntitiesSet is a shared HashSet that could be modified during foreach iteration, which is not allowed.
+		// Thus we copy HashSet to a preallocated array to avoid heap/GC allocation, and do the destruction process with that array.
+		playerStateEntitiesSet.CopyTo(s_PreallocatedPlayerStateEntitiesToBeDestroyed);
+		for (int i = 0; i < playerStateEntitiesSet.Count; i++)
+		{
+			var playerStateEntity = s_PreallocatedPlayerStateEntitiesToBeDestroyed[i];
+			playerStateEntity.Destroy();
+		}
+
+		return;
+	}
 }
--- GerritoryECS/Assets/Sources/Systems/Item/CreateSpeedChangeStateForEaterOnItemEatenSystem.cs
+++ GerritoryECS/Assets/Sources/Systems/Item/CreateSpeedChangeStateForEaterOnItemEatenSystem.cs
@@ -34,22 +34,9 @@ public sealed class CreateSpeedChangeStateForEaterOnItemEatenSystem : IFixedUpda
 			}
 
 			int stateHolderId = eaterEntity.StateHolder.Id;
-			var playerStateEntitiesSet = m_PlayerStateContext.GetEntitiesWithState(stateHolderId);
-			if (playerStateEntitiesSet.Count > 1)
-			{
-				Debug.LogWarning($"There should only be at most 1 state targetting a state holder at the same time, but there are {playerStateEntitiesSet.Count}." +
-					$"Something could be wrong!");
-			}
 
-			// We need to do copy the list because playerStateEntitiesSet is a shared HashSet that could be modified during foreach iteration, which is not allowed.
-			// Thus we copy HashSet to a preallocated array to avoid heap/GC allocation, and do the destruction process with that array.
-			playerStateEntitiesSet.CopyTo(m_PreallocatedPlayerStateEntitiesToBeDestroyed);
-			for (int i = 0; i < playerStateEntitiesSet.Count; i++)
-			{
-				var playerStateEntity = m_PreallocatedPlayerStateEntitiesToBeDestroyed[i];
-				// Remove existing states targetting the holder because there should only be 1 state at a time.
-				playerStateEntity.Destroy();
-			}
+			// Remove existing states targetting the holder because there should only be 1 state at a time.
+			m_PlayerStateContext.RemovePlayerStateFor(stateHolderId);
 
 			// Create a new state entity targetting the holder.
 			PlayerStateEntity newStateEntity = m_PlayerStateContext.CreateEntity();
