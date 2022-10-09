5a562138a3dd699418b9c2401e6390e2a2896fc7
Ta David Yu <ta.yu.gamedev@gmail.com>
2022-10-09 17:28:50 +0200

    Reset movement speed

--- GerritoryECS/Assets/Scripts/GameFlow/GameConstants.cs
+++ GerritoryECS/Assets/Scripts/GameFlow/GameConstants.cs
@@ -7,7 +7,7 @@ public static class GameConstants
 	public static readonly float TileOffset = 1;
 	public static readonly Quaternion InGameCameraOrientation = new Quaternion(-0.287274003f, -0.227902189f, 0.0705751702f, -0.927660227f);
 
-	public static readonly float MoveOnTileDuration = 0.15f;
+	public static readonly float MoveOnTileDuration = 0.33f;
 
 	public static readonly float WaitingForRespawnDuration = 3.0f;
 
