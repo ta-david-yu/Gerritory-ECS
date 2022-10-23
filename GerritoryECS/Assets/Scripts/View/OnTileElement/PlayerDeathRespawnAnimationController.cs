7a70af5145731d0752ac7ed7a6c1ebad2c03cd75
Ta David Yu <ta.yu.gamedev@gmail.com>
2022-10-23 20:08:50 +0200

    Added basic respawn animation (scale double out back)

--- GerritoryECS/Assets/Scripts/View/OnTileElement/PlayerDeathRespawnAnimationController.cs
+++ GerritoryECS/Assets/Scripts/View/OnTileElement/PlayerDeathRespawnAnimationController.cs
@@ -3,15 +3,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerDeathAnimationController : EntityCreationEventListenerBase, IDeadAddedListener, IDeadRemovedListener
+public class PlayerDeathRespawnAnimationController : EntityCreationEventListenerBase, IDeadAddedListener, IDeadRemovedListener
 {
 	[SerializeField]
 	private GameObject m_Appearance;
+
+	[SerializeField]
+	private Transform m_RespawnAnimationRoot;
+
+	[Header("Settings")]
+
+	[SerializeField]
+	private float m_RespawnAnimationStartScale = 0.0f;
+
+	[SerializeField]
+	private float m_RespawnAnimationEndScale = 1.0f;
+
+	[SerializeField]
+	private float m_RespawnAnimationDuration = 0.2f;
+
+	[SerializeField]
+	private EasingFunction.Ease m_RespawnAnimationEasingType = EasingFunction.Ease.EaseOutBackDouble;
+
+	private Tweener m_RespawnTweener = null;
 
 	/// <summary>
 	/// For debug purpose
 	/// </summary>
 	private ElementEntity m_CahcedGameEntity;
+
 
 	public override void HandleOnEntityCreated(Contexts contexts, IEntity entity)
 	{
@@ -32,12 +52,27 @@ public class PlayerDeathAnimationController : EntityCreationEventListenerBase, I
 
 	public void OnDeadAdded(ElementEntity entity)
 	{
+		// Dead animation
 		m_Appearance.SetActive(false);
 	}
 
 	public void OnDeadRemoved(ElementEntity entity)
 	{
+		// Respawn animation
 		m_Appearance.SetActive(true);
+
+		Tweener.SafeAbortTweener(ref m_RespawnTweener);
+
+		m_RespawnTweener = TweenManager.Instance.Tween((float progress) =>
+		{
+			Vector3 newScale = Vector3.one * Mathf.LerpUnclamped(m_RespawnAnimationStartScale, m_RespawnAnimationEndScale, progress);
+			m_RespawnAnimationRoot.localScale = newScale;
+
+		}).SetEase(m_RespawnAnimationEasingType).SetTime(m_RespawnAnimationDuration)
+		.SetEndCallback(() =>
+		{
+			m_RespawnTweener = null;
+		});
 	}
 
 	[ContextMenu("Kill")]
